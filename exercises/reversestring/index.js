3d1538e608a6e0bee3e53cbe617320f358066ef0
slurio <samantha.lurio@gmail.com>
2020-11-17 16:58:22 -0500

    done reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,40 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+//My solution below
+function reverse(str) {
+    let reverseWord = ''
+// avoid using below syntax
+    for(i=0; i < str.length; i++){
+        letter = str[i]
+        reverseWord = letter + reverseWord
+    }
+    return reverseWord
+}
+
 
 module.exports = reverse;
+
+// His solution: reverse() for arrays 
+// function reverse(str) {
+    // const array = str.split('');
+    // array.reverse();
+    // return array.join('');
+//     return str.split('').reverse().join('')
+// }
+
+// Additional solution
+// function reverse(str) {
+//     let reversed = '';
+    //use for of loop
+//     for(let character of str) {
+//         reversed = character + reversed
+//     }
+
+//     return reversed
+// }
+
+//Additional solution to WOW them
+// function reverse(str) {
+//     return str.split('').reduce((rev, char) => char + rev, '')
+// }
