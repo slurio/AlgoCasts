3c35a1eb27253e25316bf0f86de182e89c5b81b3
slurio <samantha.lurio@gmail.com>
2020-11-20 15:02:36 -0500

    done palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,41 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+//My solution but cleaned up true/false 
+function palindrome(str) {
+    reverse = ''
+    for(const character of str){
+        reverse = character + reverse
+    }
+    
+    return reverse === str
+}
 
 module.exports = palindrome;
+
+//My solution
+// function palindrome(str) {
+//     reverse = ''
+//     for(const character of str){
+//         reverse = character + reverse
+//     }
+//     if(reverse === str){
+//         return true
+//     }else {
+//         return false
+//     }
+// }
+
+//His 1st solution
+// function palindrome(str) {
+//     const reversed = str.split('').reverse().join('')
+
+//     return str === reversed
+// }
+
+//His 2nd solution but not the best way to solve it 
+// function palindrome(str) {
+//     return str.split('').every((char, index)=>{
+//         return char === str[str.length - index - 1]
+//     })
+// }
