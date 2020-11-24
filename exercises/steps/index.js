3415beaa193aad0f64c623889fc1c242d654e65d
slurio <samantha.lurio@gmail.com>
2020-11-24 10:47:27 -0500

    steps done

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,55 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
 
+//for loop to iterate through n
+//count index/loop to figure out how many # to print
+//until it reaches length === n
+
+// Solution Two (Not Recommended) :
+// Recursive Solution
+function steps(n, row = 0, stair = '') {
+   //define base case 1st
+    if (n === row ) {
+       return
+    }
+
+    if (n === stair.length) {
+       console.log(stair)
+       steps(n, row + 1)
+       return
+       // or return steps(n, row + 1)
+    }
+
+    if (stair.length <= row) {
+        stair += '#'
+    } else {
+        stair += ' '
+    }
+    steps (n, row, stair)
+}
+ 
 module.exports = steps;
+
+// Solution One (Recommended):
+// From 0 to n (iterate through rows) (row is one string set)
+// Create an emptry string 'stair'
+// From 0 to n (iterate through columns)
+// IF current column <= to current row add # to stair
+// ELSE add space to stair
+// console.log stair
+// function steps(n) {
+//     for (let row = 0; row < n; row++) {
+//         let stair = ''
+         
+//         for (let column = 0; column < n; column ++) {
+//             if (column <= row) {
+//                 stair += '#'
+//             } else {
+//                 stair += ' '
+//             }
+//         }
+
+//         console.log(stair)
+//     }
+// }
