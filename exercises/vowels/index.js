61aef0c9e87a21687b80d30ddf66df8d81ddd9f5
slurio <samantha.lurio@gmail.com>
2020-11-25 17:44:15 -0500

    done vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,39 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+//2nd Solution using a regular expression
+function vowels(str) {
+    //match experssion returns array if found / if not found returns null
+   const matches =  str.match(/[aeiou]/gi)
+   return matches ? matches.length : 0
+}
 
 module.exports = vowels;
+
+//My Solution
+// function vowels(str) {
+//     let count = 0
+//     for(let letter of str.toLowerCase()) {
+//         if (letter === 'a' || letter === 'e' || letter === 'i' || letter === 'o' || letter === 'u' ) {
+//             count += 1
+//         }
+//     }
+//     return count
+// }
+
+//1st Solution
+// function vowels(str) {
+//     let count = 0
+//     // const checker = 'aeiou'
+//     // more clear to use array
+//     const checker = ['a', 'e', 'i', 'o', 'u']
+
+//     for (let char of str.toLowerCase()) {
+//         //helper method
+//         if (checker.includes(char)) {
+//             count ++
+//         }
+//     }
+
+//     return count
+// }
